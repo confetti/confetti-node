71405acea1868d5df43142fb897a99a936279cf1
Jonny Strömberg <jonny.stromberg@gmail.com>
2020-03-11 22:11:13 +0100

    Change sample import since zapier don’t allow dynamic require

--- src/models/samples/index.js
+++ src/models/samples/index.js
@@ -1,7 +1,38 @@
+const samples = {
+  category: {
+    raw: require('./category/raw'),
+    formatted: require('./category/formatted')
+  },
+  event: {
+    raw: require('./event/raw'),
+    formatted: require('./event/formatted')
+  },
+  image: {
+    raw: require('./image/raw'),
+    formatted: require('./image/formatted')
+  },
+  payment: {
+    raw: require('./payment/raw'),
+    formatted: require('./payment/formatted')
+  },
+  ticket: {
+    raw: require('./ticket/raw'),
+    formatted: require('./ticket/formatted')
+  },
+  webhook: {
+    raw: require('./webhook/raw'),
+    formatted: require('./webhook/formatted')
+  },
+  workspace: {
+    raw: require('./workspace/raw'),
+    formatted: require('./workspace/formatted')
+  }
+}
+
 module.exports = function() {
   const getSample = model => {
-    const raw = require(`./${model}/raw`)
-    const formatted = require(`./${model}/formatted`)
+    const raw = samples[model].raw
+    const formatted = samples[model].formatted
     const rawMultiple = { data: [raw.data, raw.data] }
     const formattedMultiple = [
       JSON.parse(JSON.stringify(formatted)),
